344e39268970ca776ea1b0cfa711484d16d6d13a
Fransys1234 <ciocarlangalina1971@gmail.com>
2024-02-29 21:14:18 +0200

    commit setup

--- quill/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-
-export default function Home() {
-  return (
-    <MaxWidthWrapper className="bg-red-500"><div>alex</div></MaxWidthWrapper>
-  );
-}
